a597ab0774230f69d4ee725c74d62a04cd2ef5f1
itaassaas <54752417+itaassaas@users.noreply.github.com>
2020-09-16 13:57:33 -0500

    up

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 /*Kartero App Configuration*/
 
 var krms_driver_config ={	
-	'ApiUrl':"",						
-	'DialogDefaultTitle':"Kartero",    
-	'APIHasKey':"",
+	'ApiUrl':"https://tuboxv1.tupedido.app/api",						
+	'DialogDefaultTitle':"TuBoxv1",    
+	'APIHasKey':"KeyColombia2019**",
 	'debug': false
 };
